c47417715f6f1f9c1542e88e54e74c7840440f5f
Adrien <adrien.payet@outlook.com>
2021-03-15 18:17:51 +0100

    Refactoring et lissage graphique

--- assets/app.js
+++ assets/app.js
@@ -1,22 +1,52 @@
+let texte
+let reglages
+
 document.addEventListener('DOMContentLoaded', () => {
 
     // =========================================== OBJETS
 
-    const texte = {
+    texte = {
         conteneur: document.querySelector('.conteneurTexte'),
         origine: document.querySelector('.conteneurTexte').dataset.origine,
         partition: '',
         preparerPartition: () => {
-            texte.partition = texte.origine.replaceAll('()', '<span class="silence soupir"></span>').replaceAll('(-)', '<span class="silence pause"></span>')
+            texte.partition = texte.origine.replaceAll('()()', '<span class="silence pause"></span>').replaceAll('()', '<span class="silence soupir"></span>')
         },
         installerPartition: partition => {
             texte.conteneur.innerHTML = texte.partition
+        },
+        nettoyer: () => {
+            return texte.origine.replaceAll('()()', '').replaceAll('()', '')
+        },
+        prendrePonctuations: () => {
+            let texteNettoye = texte.nettoyer()
+
+            const regExpPoints = /\./g
+            const points = Array.from(texteNettoye.matchAll(regExpPoints))
+            
+            const regExpPointsInterrogation = /\? (?!»)/g
+            const pointsInterrogation = Array.from(texteNettoye.matchAll(regExpPointsInterrogation))
+
+            const regExpGuillemets = /\»/g
+            const guillemets = Array.from(texteNettoye.matchAll(regExpGuillemets))
+            
+            const ponctuations = points.concat(pointsInterrogation, guillemets)
+            
+            ponctuations.sort((a, b) => {
+                return a['index'] - b['index']
+            })
+
+            return ponctuations
         }
     }
 
-    const reglages = {
+    reglages = {
+        conteneur: document.querySelector('.conteneurReglages'),
         rythme: 0,
-        boutons: document.querySelectorAll('button'),
+        boutons: {
+            panneau: document.querySelector('.basculerPanneau'),
+            reglages: document.querySelectorAll('.reglages')
+        },
         couper: () => {
             if (reglages.rythme < 2) {
                 reglages.rythme++
@@ -48,20 +78,61 @@ document.addEventListener('DOMContentLoaded', () => {
                 }
             }
         },
-        essayer: () => {
-            const regExpPoints = /\./g
-            const points = Array.from(texte.origine.matchAll(regExpPoints))
-            let essai = texte.origine
-            points.forEach(point => {
-                const fin = point.index + 1
+        deconstruire: () => {
+
+            const ponctuations = texte.prendrePonctuations()
+            let deconstruction = texte.nettoyer()
+
+            ponctuations.forEach((ponctuation, position) => {
+                const fin = position === 0 ? ponctuation.index + 1 : ponctuation.index + ((position * 8)+1)
                 console.log(`Fin : ${fin}`)
 
-                const partieUne = texte.origine.slice(0, fin)
-                const partieDeux = texte.origine.slice(fin)
+                const partieUne = deconstruction.slice(0, fin)
+                const partieDeux = deconstruction.slice(fin)
 
-                essai = partieUne + '<br>' + partieDeux
+                deconstruction = partieUne + '<br><br>' + partieDeux
             })
-            console.log(essai)
+
+            texte.conteneur.innerHTML = deconstruction
+
+        },
+        essayer: () => {
+            const ponctuations = texte.prendrePonctuations()
+            let essai = texte.nettoyer()
+            let nbrCaracteresAjoutes = 0
+
+            ponctuations.forEach((ponctuation, position) => {
+                let hasard = Math.floor(Math.random() * 10)
+                console.log(hasard)
+                if (hasard <= 1) {
+                    console.log(`Hasard : ${hasard} = saut de ligne`);
+                    const fin = position === 0 ? ponctuation.index + 1 : ponctuation.index + (nbrCaracteresAjoutes + 1)
+                    console.log(`Fin : ${fin}`)
+
+                    const partieUne = essai.slice(0, fin)
+                    const partieDeux = essai.slice(fin)
+
+                    nbrCaracteresAjoutes += 8 
+                    essai = partieUne + '<br><br>' + partieDeux
+                } else if (hasard > 1 && hasard <=3) {
+                    console.log(`Hasard : ${hasard} = alinéa`);
+                    const fin = position === 0 ? ponctuation.index + 1 : ponctuation.index + (nbrCaracteresAjoutes + 1)
+                    console.log(`Fin : ${fin}`)
+
+                    const partieUne = essai.slice(0, fin)
+                    const partieDeux = essai.slice(fin)
+
+                    nbrCaracteresAjoutes += 4 
+                    essai = partieUne + '<br>' + partieDeux
+                } else {
+                    console.log(`Hasard : ${hasard} = rien`);
+                }
+            })
+            
+            texte.conteneur.innerHTML = essai
+        },
+        copier: () => {
+            navigator.clipboard.writeText(texte.conteneur.innerHTML)
         }
     }
 
@@ -72,29 +143,51 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // =========================================== INTERACTIONS
 
-    reglages.boutons.forEach(bouton => {
-        bouton.addEventListener('mouseenter', () => {
-            bouton.textContent = bouton.dataset.action
+    reglages.boutons.reglages.forEach(bouton => {
+        bouton.addEventListener('mouseenter', cible => {
+            cible.target.querySelectorAll('.contenuBtn').forEach(contenu => {
+                contenu.classList.toggle('contenuBtn--action')
+            })
         })
-        bouton.addEventListener('mouseout', () => {
-            bouton.textContent = bouton.dataset.signe
+        bouton.addEventListener('mouseleave', cible => {
+            cible.target.querySelectorAll('.contenuBtn').forEach(contenu => {
+                contenu.classList.toggle('contenuBtn--action')
+            })
         })
 
         bouton.addEventListener('click', () => {
-            if (bouton.dataset.action === 'couper') {
-                reglages.couper()
-            }
-            
-            if (bouton.dataset.action === 'reunir') {
-                reglages.reunir()
+
+            switch (bouton.dataset.action) {
+                case 'origine':
+                    texte.preparerPartition()
+                    texte.installerPartition()
+                    break
+                case 'couper':
+                    reglages.couper()
+                    break
+                case 'reunir':
+                    reglages.reunir()
+                    break
+                case 'deconstruire':
+                    reglages.deconstruire()
+                    break
+                case 'essayer':
+                    reglages.essayer()
+                    break
+                case 'copier':
+                    reglages.copier()
+                    break
             }
 
-            if (bouton.dataset.action === 'essayer') {
-                reglages.essayer()
-            }
-
-            console.log(`Rythme : ${reglages.rythme}`);
+            // console.log(`Rythme : ${reglages.rythme}`);
         })
+    })
+
+    reglages.boutons.panneau.addEventListener('click', () => {
+        reglages.conteneur.classList.toggle('conteneurReglages--ouvert')
+        reglages.boutons.panneau.classList.toggle('basculerPanneau--ouvert')
+        document.querySelector('.chevron').classList.toggle('chevron--ouvert')
+        texte.conteneur.classList.toggle('conteneurTexte--centre')
     })
 
 // =========================================== FIN
--- site/templates/default.php
+++ site/templates/default.php
@@ -10,13 +10,35 @@
     </head>
     <body>
         <div class="conteneurGlobal">
-            <div class="conteneur conteneurTexte" data-origine="<?= $site->texte()->kt() ?>">
+            <div class="conteneur conteneurTexte conteneurTexte--centre" data-origine="<?= $site->texte()->kt() ?>">
             </div>
             <div class="conteneur conteneurReglages">
                 <h1>Rythme</h1>
-                <button data-action="couper" data-signe="– / –">– / –</button>
-                <button data-action="reunir" data-signe="—">—</button>
-                <button data-action="essayer" data-signe="?">?</button>
+                <button class="basculerPanneau">
+                    <span class="chevron"></span>
+                    <!-- <img src="<?= $kirby->url('assets') ?>/svg/arrow.svg" alt="">     -->
+                </button>
+
+                <button class="reglages" data-action="origine" data-signe="o">
+                    <div class="contenuBtn">o</div>
+                    <div class="contenuBtn">origine</div>
+                </button>
+                <button class="reglages" data-action="couper" data-signe="– / –">
+                    <div class="contenuBtn">– / –</div>
+                    <div class="contenuBtn">couper</div>
+                </button>
+                <button class="reglages" data-action="reunir" data-signe="—">
+                    <div class="contenuBtn">—</div>
+                    <div class="contenuBtn">réunir</div>
+                </button>
+                <button class="reglages" data-action="deconstruire" data-signe="- - -">
+                    <div class="contenuBtn">- - -</div>
+                    <div class="contenuBtn">déconstruire</div>
+                </button>
+                <button class="reglages" data-action="essayer" data-signe="?">
+                    <div class="contenuBtn">?</div>
+                    <div class="contenuBtn">essayer</div>
+                </button>
             </div>
         </div>
     </body>
